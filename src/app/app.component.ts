5b083de9490981beb28cf3fc45f51d015eb02968
Arkopal <arko466@gmail.com>
2023-08-25 00:29:25 +0530

    css fixes and two way data bind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'taskSubtask';
+export class AppComponent implements OnInit{
+  title = '';
+  arr: any = []
+
+  onSubmit(){
+
+    if(!this.title || this.title !== "Add a Task" ) {
+      this.arr.push(this.title)
+      this.title = ""
+    console.log(this.arr)
+
+    }
+    else {
+      alert("please provide proper input")
+    }
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
