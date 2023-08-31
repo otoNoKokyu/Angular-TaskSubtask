bfadd0e7542daf97b47d44d2ebc5c7a4e3450a08
Arkopal <arko466@gmail.com>
2023-08-31 22:13:59 +0530

    signup component initialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SubTaskComponent } from './sub-task/sub-task.component';
 import { TaskComponent } from './task/task.component';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 const routes: Routes = [
   { path: '', component: LoginComponent }, // Default route
   { path: 'task', component: TaskComponent }, // Default route
@@ -18,7 +19,8 @@ const routes: Routes = [
     AppComponent,
     SubTaskComponent,
     TaskComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
