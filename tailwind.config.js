099e1be565f70d284035dcc21b70d7194191d771
Arkopal <arko466@gmail.com>
2023-09-13 22:45:19 +0530

    responsive signup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@ module.exports = {
   corePlugins: {
     preflight: false,
   },
+
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    screens:{
+      'sm': '320px',
+      'md': '500px'
+    },
     extend: {
       width: {
         '98.5': '98.4%',
